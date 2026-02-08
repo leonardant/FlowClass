8368afff7fcc35fd9c2b6a337aaafdcb575480d3
NEW-DELL-PC\aleonard <leonard.ant@gmail.com>
2026-02-08 17:33:25 +0000

    Added Step "D"

---
+++ FlowClass/Steps/StepD.cs
@@ -0,0 +1,21 @@
+﻿using FlowClass.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowClass.Steps
+{
+    public class StepD : ILifecycleStep<Order>
+    {
+        public string Name => "D";
+
+        public void Execute(Order context)
+        {
+            context.Status = "Processed D";
+            context.History.Add("Step D executed");
+            Console.WriteLine($"Order {context.Id}: Step D");
+        }
+    }
+}
